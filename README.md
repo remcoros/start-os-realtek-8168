fe110f591afbbbad48e54ce31192d87cbf756266
Remco Ros <remcoros@live.nl>
2025-01-10 16:48:57 +0000

    initial release

---
+++ README.md
@@ -0,0 +1,25 @@
+# Modded Start OS 0.3.5.1 with Realtek 8168 support
+
+This is a modded release of [Start OS v0.3.5.1 (x86_64-nonfree)](https://github.com/Start9Labs/start-os/releases/tag/v0.3.5.1) that adds support for the Realtek 8168 PCI-Express Ethernet controller.
+
+It installs the [r8168-dkms](https://packages.debian.org/bookworm/r8168-dkms) kernel driver inside the original iso.
+
+# Source / build yourself
+
+See: [startos.sh](startos.sh) (run with bash)
+
+# Acknowledgement
+
+Thanks Start9 team for the great software and open-source license.
+
+Thanks TK for testing!
+
+# Links / References
+
+- [Start OS](https://github.com/Start9Labs/start-os) / [Start9](https://start9.com/)
+- [Customizing a live ISO image](https://wiki.debian.org/RepackBootableISO)
+- [Repacking a Debian ISO with its original boot equipment](https://ccoff.github.io/customizing-live-iso-image)
+
+# License
+
+[MIT](LICENSE)
---
+++ startos.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+set -ea
+
+# the current and new name of the .iso file
+isofile=startos-0.3.5.1-39de098_x86_64-nonfree.iso
+newisofile=startos-0.3.5.1-39de098_x86_64-nonfree-r8168.iso
+
+# install required dependencies for building an iso
+echo "Installing dependencies..."
+apt install xorriso squashfs-tools
+
+# download iso file if it doesn't exist yet
+echo "Downloading start-os iso..."
+test -e $isofile || \
+    wget https://github.com/Start9Labs/start-os/releases/download/v0.3.5.1/$isofile
+
+# verify checksum of start-os iso file
+echo "29b0f1e0211568ea66d6b729536ff84aed3b6cbc1c38540a7c80a391fd01616e $isofile" | sha256sum --check --status || (echo "invalid checksum" && exit 1)
+
+# mount the iso and containing filesystem squashfs 
+echo "Preparing..."
+umount squashfs
+umount iso
+
+rm $newisofile
+rm -r iso && mkdir iso
+rm -r squashfs && mkdir squashfs
+rm -r squashfs-mod && mkdir squashfs-mod
+
+mount -o loop $isofile iso
+mount -o loop iso/live/filesystem.squashfs squashfs
+
+# mounting an iso/squashfs file is read-only, copy 'squashfs' dir so we can modify it
+echo "Copy squashs into squashfs-mod..."
+rsync -a squashfs/ squashfs-mod/
+
+# now chroot into filesystem and install driver from debian snapshot repository
+# the snapshot date matches the date around when start-os 0.3.5.1 was build
+# we do this to prevent updating the kernel version due to the dkms tools
+echo "Installing driver..."
+chroot squashfs-mod /bin/bash -x <<'EOT'
+mount -t proc none /proc/
+mount -t sysfs none /sys/
+
+echo "deb [check-valid-until=no] https://snapshot.debian.org/archive/debian/20231121T031647Z stable main contrib non-free non-free-firmware" > /etc/apt/sources.list
+apt update && apt install -y linux-headers-6.1.0-13-amd64 r8168-dkms
+
+apt clean
+rm -rf /tmp/*
+umount /proc/
+umount /sys/
+EOT
+
+# make a new squashfs filesystem
+echo "Creating new squashfs filesystem..."
+rm filesystem-mod.squashfs
+mksquashfs squashfs-mod filesystem-mod.squashfs
+
+# create a new iso based on existing with updated filesystem and kernel files
+echo "Creating iso..."
+
+xorriso \
+   -indev $isofile \
+   -overwrite "on" \
+   -compliance no_emul_toc \
+   -update filesystem-mod.squashfs live/filesystem.squashfs \
+   -update squashfs-mod/boot/initrd.img-6.1.0-13-amd64 live/initrd.img \
+   -update squashfs-mod/boot/vmlinuz-6.1.0-13-amd64 live/vmlinuz \
+   -map squashfs-mod/boot/initrd.img-6.1.0-13-amd64 live/initrd.img-6.1.0-13-amd64 \
+   -map squashfs-mod/boot/vmlinuz-6.1.0-13-amd64 live/vmlinuz-6.1.0-13-amd64 \
+   -map squashfs-mod/boot/System.map-6.1.0-13-amd64 live/System.map-6.1.0-13-amd64 \
+   -outdev $newisofile \
+   -boot_image any replay \
+   -padding included
+
+# some cleanup
+echo "Cleaning up..."
+
+umount squashfs
+umount iso
+rm -rf squashfs-mod
+rm -rf squashfs
+rm -rf iso
+rm filesystem-mod.squashfs
+
+echo "Done"
